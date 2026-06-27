6e5d872dd47a5e12b8f78724ecede9c06160b929
dlwotjd02260226-cmd <dlwotjd02260226@gmail.com>
2026-06-27 17:04:42 +0900

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import warnings, os, json
 warnings.filterwarnings("ignore")
 os.environ["STREAMLIT_SERVER_SUPPRESS_NOWARNINGS"] = "true"
@@ -65,7 +66,7 @@ if key in st.session_state: del st.session_state[key]
 st.rerun()
 
 ==========================================
-[★] API Key 및 패스워드 설정 구간
+[]API Key 및 패스워드 설정 구간]
 ==========================================
 API_CONFIG = {
 'apiKey': '600930d1-7207-4939-901b-df2d608f5035',

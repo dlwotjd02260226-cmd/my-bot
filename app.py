470830db94d400bf9d2e300a7c88413689c4127c
dlwotjd02260226-cmd <dlwotjd02260226@gmail.com>
2026-07-06 18:39:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def get_price():
 price = get_price()
 
 # 제목 글자 크기 조절
-st.markdown("<div style='font-size: 24px; font-weight: bold; margin-bottom: 20px;'>BTC 실시간 트레이딩</div>", unsafe_allow_html=True)
+st.markdown("<div style='font-size: 42px; font-weight: bold; margin-bottom: 20px;'>BTC 실시간 트레이딩</div>", unsafe_allow_html=True)
 
 # 실전/가상 매매 및 교차/격리 모드 선택
 col_mode1, col_mode2 = st.columns(2)

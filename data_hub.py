9c7b885bbfa6cddf2e47f71956916f8fffa388bf
dlwotjd02260226-cmd <dlwotjd02260226@gmail.com>
2026-07-13 01:37:24 +0900

    Update data_hub.py

--- data_hub.py
+++ data_hub.py
@@ -1,1 +1,16 @@
+import threading
 
+class DataHub:
+    def __init__(self):
+        self.data = {}
+        self.lock = threading.Lock()
+
+    def update(self, key, value):
+        with self.lock:
+            self.data[key] = value
+
+    def get(self, key):
+        with self.lock:
+            return self.data.get(key, None)
+
+hub = DataHub()
